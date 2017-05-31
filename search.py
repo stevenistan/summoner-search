bc0f4d999ff9722e2a4d33dd54c426574c303958
Steven Tan <tan.steven.97@gmail.com>
2017-05-31 16:16:45 -0700

    edited prompt

--- search.py
+++ search.py
@@ -15,26 +15,22 @@ def requestRankedData(region, ID, APIKey):
     return response.json()
     
 def main():
-    print("\nWhat up homie. Enter your region to get started")
-    print("Type in one of the following regions or else the program wont work correctly:\n")
-    print("NA or EUW (Note: You can add more regions by just changing up the URL!\n")
-
-    region = (str)(input('Type in one of the regions above: '))
+    region = 'NA'
     summonerName = (str)(input('Type your Summoner Name here and DO NOT INCLUDE ANY SPACES: '))
-    APIKey = (str)(input('Copy and paste your API Key here: '))
+    APIKey = 'RGAPI-1b438410-0037-4a9f-9989-d9ed18238786'
 
     #I send these three pieces off to my requestData function which will create the URL and give me back a JSON that has the ID for that specific summoner.
     #Once again, what requestData returns is a JSON.
-    responseJSON  = requestSummonerData(region, summonerName, APIKey)
+    responseJSON = requestSummonerData(region, summonerName, APIKey)
     
     ID = responseJSON[summonerName]['id']
     ID = str(ID)
     print(ID)
     responseJSON2 = requestRankedData(region, ID, APIKey)
-    print(responseJSON2[ID][0]['tier'])
-    print(responseJSON2[ID][0]['entries'][0]['division'])
-    print(responseJSON2[ID][0]['entries'][0]['leaguePoints'])
+    print(summonerName + " is rank: "
+          + responseJSON2[ID][0]['tier'] + " "
+          + responseJSON2[ID][0]['entries'][0]['division'] + " "
+          + str(responseJSON2[ID][0]['entries'][0]['leaguePoints']))
 
 if __name__ == "__main__":
     main()
-

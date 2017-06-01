a94c387a4154a8baf7a896724cc463f9fe2be46c
Steven Tan <tan.steven.97@gmail.com>
2017-06-01 11:45:45 -0700

    Changed API key

--- search.py
+++ search.py
@@ -1,29 +1,29 @@
 import requests
 
-def requestSummonerData(region, summonerName, APIKey):    
+def request_summoner_data(region, summonerName, APIKey):    
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + APIKey
     response = requests.get(URL)
     return response.json()
 
-def requestRankedData(region, ID, APIKey):
+def request_ranked_data(region, ID, APIKey):
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.5/league/by-summoner/" + ID + "/entry?api_key=" + APIKey
     response = requests.get(URL)
     return response.json()
     
 def main():
     region = 'na'
-    summonerName = (str)(input('Type your Summoner Name here: '))
+    summonerName = (str)(input('Type your Summoner Name (NA) here: '))
     summonerName = summonerName.lower().replace(" ", "")
-    APIKey = 'RGAPI-1b438410-0037-4a9f-9989-d9ed18238786'
+    APIKey = 'RGAPI-1b438410-0037-4a9f-9989-d9ed18238786' # OLD KEY
 
-    responseJSON = requestSummonerData(region, summonerName, APIKey)
+    responseJSON = request_summoner_data(region, summonerName, APIKey)
     
     ID = responseJSON[summonerName]['id']
     ID = str(ID)
     print(ID)
     name = responseJSON[summonerName]['name']
-    responseJSON2 = requestRankedData(region, ID, APIKey)
-    print(name + " is rank: "
+    responseJSON2 = request_ranked_data(region, ID, APIKey)
+    print(name + " is ranked "
           + responseJSON2[ID][0]['tier'] + " "
           + responseJSON2[ID][0]['entries'][0]['division'] + " "
           + str(responseJSON2[ID][0]['entries'][0]['leaguePoints'])
